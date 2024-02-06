f273184804542ed65832f0db657236b9db4b24a9
zarfld <dominik.zarfl@avl.com>
2024-02-06 15:59:07 +0100

    Copy the example configs

--- install.sh
+++ install.sh
@@ -10,3 +10,12 @@ echo "Executing prerequisites.sh script..."
 # Execute the compile.sh script
 echo "Executing compile.sh script..."
 /bin/bash "${SCRIPT_DIR}/compile.sh"
+
+# Ensure that the folder for example configs is existing
+if [ ! -d "/usr/share/doc/linuxcnc/examples/sample-configs/by_interface/pokeys" ]; then
+    mkdir -p /usr/share/doc/linuxcnc/examples/sample-configs/by_interface/pokeys
+fi
+
+# Copy the example configs to the correct folder
+echo "Copying example configs to the correct folder..."
+cp -r "./DM542_XXYZ_mill" /usr/share/doc/linuxcnc/examples/sample-configs/by_interface/pokeys
