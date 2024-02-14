c9b86b148a8d555a27763ce74c29399369050a4b
zarfld <dominik.zarfl@avl.com>
2024-02-14 12:56:31 +0100

    adapt compile.sh

--- compile.sh
+++ compile.sh
@@ -14,6 +14,8 @@ echo "pokeys_homecomp.comp compiled successfully."
 
 echo "preparing prerequisites of pokeys_rt.comp..."
 cp ../pokeyslib/Makefile.noqmake ../pokeyslib/Makefile.noqmake.rt
+cp ../pokeyslib/PoKeysLib.h ../pokeyslib/PoKeysLibRt.h
+
 #modify the Makefile.noqmake.rt to exclude libusb-1.0
 sed -i 's/ -lusb-1.0//g' ../pokeyslib/Makefile.noqmake.rt
 
@@ -29,26 +31,23 @@ sed -i 's/hid-libusb.c //g' ../pokeyslib/Makefile.noqmake.rt
 sed -i 's/PoKeysLibFastUSB.c //g' ../pokeyslib/Makefile.noqmake.rt
 
 #modify the Makefile.noqmake.rt to change line 'cp PoKeysLib.h /usr/include' to 'cp PoKeysLib.h  /usr/include/linuxcnc'
-sed -i 's/cp PoKeysLib.h \/usr\/include/cp PoKeysLib.h  \/usr\/include\/linuxcnc/g' ../pokeyslib/Makefile.noqmake.rt
+sed -i 's/cp PoKeysLib.h \/usr\/include/cp PoKeysLibRt.h  \/usr\/include\/linuxcnc/g' ../pokeyslib/Makefile.noqmake.rt
 
-#add lines folloing line in the begining of ../pokeyslib/pokeyslib.h
-#  #ifdef POKEYSLIB_USE_LIBUSB
-#    #undef POKEYSLIB_USE_LIBUSB
-#  #endif
-sed -i 's/#define POKEYSLIB_USE_LIBUSB/#ifdef POKEYSLIB_USE_LIBUSB\n#undef POKEYSLIB_USE_LIBUSB\n#endif\n#define POKEYSLIB_USE_LIBUSB/g' ../pokeyslib/pokeyslib.h
+#add line '#undef POKEYSLIB_USE_LIBUSB' in the begining of ../pokeyslib/PoKeysLibRt.h
+sed -i '1s/^/#undef POKEYSLIB_USE_LIBUSB\n/' ../pokeyslib/PoKeysLibRt.h
 
 # copy pokeys_rt.comp to ../pokeyslib
 cp pokeys_rt.comp ../pokeyslib
 
-# copy ../pokeyslib/Makefile.noqmake.rt and ../pokeyslib/PoKeysLib.h to current folder
+# copy ../pokeyslib/Makefile.noqmake.rt and ../pokeyslib/PoKeysLibRt.h to current folder
 cp ../pokeyslib/Makefile.noqmake.rt .
-cp ../pokeyslib/PoKeysLib.h .
+cp ../pokeyslib/PoKeysLibRt.h .
 cp ../libPoKeys.so .
 
-# stage, commit and push  Makefile.noqmake.rt and PoKeysLib.h
+# stage, commit and push  Makefile.noqmake.rt and PoKeysLibRt.h
 #git add Makefile.noqmake.rt
-#git add PoKeysLib.h
-#git commit -m "Makefile.noqmake.rt and PoKeysLib.h updated for pokeys_rt.comp"
+#git add PoKeysLibRt.h
+#git commit -m "Makefile.noqmake.rt and PoKeysLibRt.h updated for pokeys_rt.comp"
 #git push
 
 # compile pokeyslib and install it to /usr/lib/linuxcnc/modules
