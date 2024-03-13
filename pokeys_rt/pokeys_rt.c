a7f0550dbf360e13c2fc69660aff4603df8f04d3
zarfld <dominik.zarfl@avl.com>
2024-03-13 10:20:24 +0100

    deb

--- pokeys_rt/pokeys_rt.c
+++ pokeys_rt/pokeys_rt.c
@@ -40,8 +40,8 @@ option extra_setup;
 #define XSTR(s) STR(s)
 #undef POKEYSLIB_USE_LIBUSB
 //#include "/usr/include/linuxcnc/motion.h"
-//#include "/usr/include/linuxcnc/homing.h"
-#include "/usr/include/linuxcnc/rtapi.h"
+#include "/usr/include/linuxcnc/homing.h"
+#include "rtapi.h"
 #include "/usr/include/linuxcnc/rtapi_ctype.h"	/* isspace() */
 #include "/usr/include/linuxcnc/rtapi_app.h"		/* RTAPI realtime module decls */
 
