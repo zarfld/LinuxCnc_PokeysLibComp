104e486a3ffd85d13e4e7a9a0ef22aaf095eced4
zarfld <dominik.zarfl@avl.com>
2024-03-13 15:35:05 +0100

    deb

--- pokeys_rt/pokeys_rt.c
+++ pokeys_rt/pokeys_rt.c
@@ -39,7 +39,7 @@ option extra_setup;
 #define STR(s)  #s
 #define XSTR(s) STR(s)
 #undef POKEYSLIB_USE_LIBUSB
-#include "motion.h"
+#include "/usr/include/linuxcnc/motion.h"
 #include "homing.h"
 #include "rtapi.h"
 #include "rtapi_ctype.h"	/* isspace() */
