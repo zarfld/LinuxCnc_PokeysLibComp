a643b3961a712c54672a0c990a120d07bc405b67
zarfld <d.zarfl72@gmail.com>
2024-09-27 18:50:30 +0200

    Remove unnecessary dependencies and add required ones in `prerequisites.sh`
    * Remove `linuxcnc-dev` from the list of dependencies
    * Add `libreadline-dev` to the list of dependencies
    * Add `python3-lxml` to the list of dependencies

--- prerequisites.sh
+++ prerequisites.sh
@@ -4,19 +4,6 @@
 echo "Updating package lists..."
 apt-get update
 
-if ! dpkg -s linuxcnc-dev >/dev/null 2>&1; then
-    # Ask user if linuxcnc-dev should be installed
-    read -p "Do you want to install linuxcnc-dev? (y/n): " install_linuxcnc_dev
-    if [ "$install_linuxcnc_dev" = "y" ]; then
-        # Installing Development Packages needed to build COMP files
-        echo "Installing linuxcnc-dev..."
-        apt-get install -y linuxcnc-dev
-    fi
-else
-    echo "Updating linuxcnc-dev..."
-    apt-get install --only-upgrade -y linuxcnc-dev
-fi
-
 # Install or update required packages
 echo "Installing or updating required packages..."
 
@@ -54,6 +41,24 @@ if ! dpkg -s libusb-1.0-0-dev >/dev/null 2>&1; then
 else
     echo "Updating libusb-1.0-0-dev..."
     apt-get install --only-upgrade -y libusb-1.0-0-dev
+fi
+
+# Check if libreadline-dev is installed
+if ! dpkg -s libreadline-dev >/dev/null 2>&1; then
+    echo "Installing libreadline-dev..."
+    apt-get install -y libreadline-dev
+else
+    echo "Updating libreadline-dev..."
+    apt-get install --only-upgrade -y libreadline-dev
+fi
+
+# Check if python3-lxml is installed
+if ! dpkg -s python3-lxml >/dev/null 2>&1; then
+    echo "Installing python3-lxml..."
+    apt-get install -y python3-lxml
+else
+    echo "Updating python3-lxml..."
+    apt-get install --only-upgrade -y python3-lxml
 fi
 
 # Change directory to parent directory
