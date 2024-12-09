5af61503673b5e3c1f9a7ad0a9db504df9b42ae6
zarfld <d.zarfl72@gmail.com>
2024-12-09 07:32:26 +0100

    Add encoder initialization and update functions for improved handling

---
+++ pokeys_uspace/PoKeysComp.h
@@ -0,0 +1,20 @@
+#ifndef POKEYSCOMP_H
+#define POKEYSCOMP_H
+
+#include "PoKeysLib.h"
+#include "rtapi.h"
+#include "hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int PKEncoder_init(int id, sPoKeysDevice *device);
+void PKEncoder_Update(sPoKeysDevice* dev);
+
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif // POKEYSCOMP_H
--- pokeys_uspace/PoKeysCompEncoders.c
+++ pokeys_uspace/PoKeysCompEncoders.c
@@ -36,7 +36,7 @@ typedef struct
     hal_float_t *position;		// pointer for "pin out float encoder.#.position[29]"
     hal_float_t *velocity;		// pointer for "pin out float encoder.#.velocity[29]"
     hal_bit_t *reset; 			// pointer for "pin in bit encoder.#.reset[29]"
-	hal_bit_t *index-enable;	// pointer for "pin in bit encoder.#.index-enable[29]"
+	hal_bit_t *index_enable;	// pointer for "pin in bit encoder.#.index-enable[29]"
 
 	hal_float_t *scale;			// pointer for "param rw float encoder.#.scale[29]  "The scale factor used to convert counts to position units. It is in “counts per position unit”""
 
@@ -58,6 +58,7 @@ typedef struct
 typedef struct
 {
     one_encoder_data_t encoder[29];
+	hal_s32_t *deb_out; // pin out s32 deb.out;
 
 } all_encoder_data_t;
 
@@ -69,28 +70,39 @@ static int makepins(int id, int njoints)
     int jno, retval;
     one_encoder_data_t *addr;
 
-    joint_home_data = hal_malloc(sizeof(all_encoder_data_t));
-    if (joint_home_data == 0)
+    encoder_data = hal_malloc(sizeof(all_encoder_data_t));
+    if (encoder_data == 0)
     {
         rtapi_print_msg(RTAPI_MSG_DBG, "PK_Encoders: all_encoder_data_t malloc failed\n");
         return -1;
     }
 
     retval = 0;
-    for (no = 0; jno < njoints; jno++)
+	retval += hal_pin_s32_newf(HAL_OUT, &(encoder_data->deb_out), id,"encoder.deb.out", jno); // create pin for "pin out s32 encoder.#.count[29]"
+		
+    for (jno = 0; jno < njoints; jno++)
     {
-        addr = &(joint_home_data->jhd[jno]);
+        addr = &(encoder_data->encoder[jno]);
 
-        retval += hal_pin_bit_newf(HAL_IN, &(addr->home_sw), id,"joint.%d.home-sw-in", jno);
-        retval += hal_pin_bit_newf(HAL_OUT, &(addr->homing), id,"joint.%d.homing", jno);
-        retval += hal_pin_bit_newf(HAL_OUT, &(addr->homed), id, "joint.%d.homed", jno);
-        retval += hal_pin_s32_newf(HAL_OUT, &(addr->home_state), id,"joint.%d.home-state", jno);
-        retval += hal_pin_bit_newf(HAL_IO, &(addr->index_enable), id,"joint.%d.index-enable", jno);
+        retval += hal_pin_s32_newf(HAL_OUT, &(addr->count), id,"encoder.%d.count", jno); // create pin for "pin out s32 encoder.#.count[29]"
+		retval += hal_pin_float_newf(HAL_OUT, &(addr->position), id,"encoder.%d.position", jno); // create pin for "pin out float encoder.#.position[29]"
+		retval += hal_pin_float_newf(HAL_OUT, &(addr->velocity), id,"encoder.%d.velocity", jno); // create pin for "pin out float encoder.#.velocity[29]"
+		retval += hal_pin_bit_newf(HAL_IN, &(addr->reset), id,"encoder.%d.reset", jno); // create pin for "pin in bit encoder.#.reset[29]"
+		retval += hal_pin_bit_newf(HAL_IO, &(addr->index_enable), id,"encoder.%d.index-enable", jno); // create pin for "pin in bit encoder.#.index-enable[29]"
+		
+	//	retval += hal_param_float_newf(HAL_RW, &(addr->scale), id,"encoder.%d.scale", jno); // create param for "param rw float encoder.#.scale[29]  "The scale factor used to convert counts to position units. It is in “counts per position unit”""
 
-        // add custom hal pins here:
-        retval += hal_pin_u32_newf(HAL_IN, &(addr->PEv2_AxesState), id,"joint.%d.PEv2.AxesState", jno);
-        retval += hal_pin_u32_newf(HAL_OUT, &(addr->PEv2_AxesCommand), id,"joint.%d.PEv2.AxesCommand", jno);
-
+		retval += hal_pin_u32_newf(HAL_IO, &(addr->encoderOptions), id,"encoder.%d.encoderOptions", jno); // create pin for "pin io unsigned Encoders.#.encoderOptions [26]"		
+													// Encoder options -    bit 0: enable encoder
+													//                      bit 1: 4x sampling
+													//                      bit 2: 2x sampling
+													//                      bit 3: reserved
+													//                      bit 4: direct key mapping for direction A
+													//                      bit 5: mapped to macro for direction A
+													//                      bit 6: direct key mapping for direction B
+													//                      bit 7: mapped to macro for direction B
+		retval += hal_pin_u32_newf(HAL_IO, &(addr->channelApin), id,"encoder.%d.channelApin", jno); // create pin for "pin io unsigned Encoders.#.channelApin [26]"         // Channel A encoder pin"
+		retval += hal_pin_u32_newf(HAL_IO, &(addr->channelBpin), id,"encoder.%d.channelBpin", jno); // create pin for "pin io unsigned Encoders.#.channelBpin [26]"         // Channel B encoder pin"
     }
     return retval;
 }
@@ -101,67 +113,74 @@ bool initEncodersDone = 0;
 bool EncoderValuesGet = false; 
 
 bool DoEncoders = true;
+unsigned int  sleepdur = 1000;
+bool  use_sleepdur1 = true;
+unsigned int  sleepdur1 = 1000;
+unsigned int  sleepdur2 = 1000;
+sPoKeysDevice * dev = NULL;
 
-public void PKEncoder_Update(sPoKeysDevice* dev)
+void PKEncoder_Update(sPoKeysDevice* dev)
 {
 
 			rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: info_iBasicEncoderCount = %d\n", __FILE__, __FUNCTION__, dev->info.iBasicEncoderCount);
 			if (dev->info.iBasicEncoderCount && DoEncoders)
 			{
-				deb_out = 216;
+				*(encoder_data->deb_out) = 216;
 				rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: PK_EncoderValuesGet(dev)\n", __FILE__, __FUNCTION__);
 				if (PK_EncoderValuesGet(dev) == PK_OK)
 				{
 					rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: PK_EncoderValuesGet(dev) OK\n", __FILE__, __FUNCTION__);
-					usleep(sleepdur);
-					deb_out = 217;
+					//usleep(sleepdur);
+					*(encoder_data->deb_out) = 217;
 					EncoderValuesGet = true;
 					bool resetEncoders = false;
 
 					resetEncoders = 0;
 					
 					/* known issue: since update to Bullseye & Lcnc 2.9 it hangs here*/
-					for (i = 0; i < dev->info.iBasicEncoderCount; i++)
+					for (int i = 0; i < dev->info.iBasicEncoderCount; i++)
 					{
 						rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: read encoder %d\n", __FILE__, __FUNCTION__, i);
-						deb_out = 218*100+i;
-						encoder_count(i) = dev->Encoders[i].encoderValue;
-						deb_out = 2180;
-						encoder_position(i) = dev->Encoders[i].encoderValue * encoder_scale(i);
-						deb_out = 2181;
-						if ((encoder_reset(i) != 0) || (initEncodersDone == false))
+						*(encoder_data->deb_out) = 218*100+i;
+						//encoder_count(i) = dev->Encoders[i].encoderValue;
+						*(encoder_data->encoder[i].count) = dev->Encoders[i].encoderValue;
+						*(encoder_data->deb_out) = 2180;
+						//encoder_position(i) = dev->Encoders[i].encoderValue * encoder_scale(i);
+						*(encoder_data->encoder[i].position) = dev->Encoders[i].encoderValue * *(encoder_data->encoder[i].scale);
+						*(encoder_data->deb_out) = 2181;
+						if ((*(encoder_data->encoder[i].reset) != 0) || (initEncodersDone == false))
 						{
-							deb_out = 2182;
+							*(encoder_data->deb_out) = 2182;
 							dev->Encoders[i].encoderValue = 0;
-							deb_out = 2183;
+							*(encoder_data->deb_out) = 2183;
 							resetEncoders = true;
-							deb_out = 2184;
+							*(encoder_data->deb_out) = 2184;
 						}
-						deb_out = 2185;
-						usleep(sleepdur);
+						*(encoder_data->deb_out) = 2185;
+						//usleep(sleepdur);
 					}
 					
 					/*
 					known issue: since update to Bullseye & Lcnc 2.9 it hangs here*/
-					deb_out = 219;
+					*(encoder_data->deb_out) = 219;
 					if (dev->info.iUltraFastEncoders)
 					{
 						rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: read UltraFastEncoders\n", __FILE__, __FUNCTION__);
-						usleep(sleepdur);
-						deb_out = 220;
+						//usleep(sleepdur);
+						*(encoder_data->deb_out) = 220;
 						
-						for (i = dev->info.iBasicEncoderCount; i < (dev->info.iBasicEncoderCount + dev->info.iUltraFastEncoders); i++)
+						for (int i = dev->info.iBasicEncoderCount; i < (dev->info.iBasicEncoderCount + dev->info.iUltraFastEncoders); i++)
 						{
 							rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: UltraFastEncoders %d\n", __FILE__, __FUNCTION__, i);
-							deb_out = 221;
-							encoder_count(i) = dev->Encoders[i].encoderValue;
-							encoder_position(i) = dev->Encoders[i].encoderValue * encoder_scale(i);
-							if ((encoder_reset(i) != 0) || (initEncodersDone == false))
+							*(encoder_data->deb_out) = 221;
+							*(encoder_data->encoder[i].count) = dev->Encoders[i].encoderValue;
+							*(encoder_data->encoder[i].position) = dev->Encoders[i].encoderValue * *(encoder_data->encoder[i].scale);
+							if ((encoder_data->encoder[i].reset != 0) || (initEncodersDone == false))
 							{
 								dev->Encoders[i].encoderValue = 0;
 								resetEncoders = true;
 							}
-							usleep(sleepdur);
+							//usleep(sleepdur);
 						}
 					}
 					/*
@@ -175,17 +194,17 @@ public void PKEncoder_Update(sPoKeysDevice* dev)
 					rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: resetEncoders = %d\n", __FILE__, __FUNCTION__, resetEncoders);
 					if (resetEncoders == true)
 					{
-						deb_out = 140;
+						*(encoder_data->deb_out) = 140;
 						rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: PK_EncoderValuesSet(dev)\n", __FILE__, __FUNCTION__);
 						if (PK_EncoderValuesSet(dev) == PK_OK)
 						{
 							rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: PK_EncoderValuesSet(dev) OK\n", __FILE__, __FUNCTION__);
-							usleep(sleepdur);
+							//usleep(sleepdur);
 							resetEncoders = false;
-							deb_out = 141;
+							*(encoder_data->deb_out) = 141;
 							initEncodersDone = true;
 						}
-						deb_out = 142;
+						*(encoder_data->deb_out) = 142;
 					}
 					
 				}
@@ -194,14 +213,23 @@ public void PKEncoder_Update(sPoKeysDevice* dev)
 }
 
 int PKEncoder_init(int id,
-                dsPoKeysDevice * device)
+                sPoKeysDevice * device)
 {
     dev = device;
-    return makepins(id, device);
+    return makepins(id, 26);
 }
 
-EXPORT_SYMBOL(PKEncoder_init);
-EXPORT_SYMBOL(PKEncoder_Update);
-EXPORT_SYMBOL(write_PKEncoder_out_pins);
-EXPORT_SYMBOL(read_PKEncoder_in_pins);
-EXPORT_SYMBOL(update_PKEncoder_params);
+int rtapi_app_main(void)
+{
+	return 0;
+}
+
+int main(void)
+{
+	return 0;
+}
+//EXPORT_SYMBOL(PKEncoder_init);
+//EXPORT_SYMBOL(PKEncoder_Update);
+//EXPORT_SYMBOL(write_PKEncoder_out_pins);
+//EXPORT_SYMBOL(read_PKEncoder_in_pins);
+//EXPORT_SYMBOL(update_PKEncoder_params);
