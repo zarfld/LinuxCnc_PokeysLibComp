4eaa15af12bb9760a1b9831646e2d9f3513e1323
Dominik Zarfl <13350047+zarfld@users.noreply.github.com>
2025-03-05 01:46:26 +1100

    PEv2_ExternalOutputsSet(dev)

--- pokeys_uspace/PoKeysCompPulseEngine_v2.c
+++ pokeys_uspace/PoKeysCompPulseEngine_v2.c
@@ -1456,7 +1456,7 @@ int32_t PEv2_ExternalOutputsSet(sPoKeysDevice* dev){
 		}
 	}
 
-	return PK_OK;
+	return ret;
 }
 
 void PKPEv2_Update(sPoKeysDevice* dev, bool HAL_Machine_On) {
@@ -2119,7 +2119,7 @@ void PKPEv2_Update(sPoKeysDevice* dev, bool HAL_Machine_On) {
 	}
 
 
-	if (PEv2_ExternalOutputsSet == PK_OK){
+	if (PEv2_ExternalOutputsSet(dev) == PK_OK){
 		rtapi_print_msg(RTAPI_MSG_DBG, "PoKeys: %s:%s: PEv2_ExternalOutputsSet=PK_OK\n", __FILE__, __FUNCTION__);
 	}
 	else {
