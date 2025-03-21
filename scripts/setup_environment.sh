1da5e5ccd5d45ca07f1ba25b9384142cbb9a7a06
Dominik Zarfl <13350047+zarfld@users.noreply.github.com>
2025-03-22 10:25:30 +1100

    Add setup_environment.sh script to export secrets as environment variables
    * Export `POKEYS57CNC_ID` secret as `POKEYS57CNC_ID` environment variable
    * Export `POKEYS57E_ID` secret as `POKEYS57E_ID` environment variable

---
+++ scripts/setup_environment.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Export PoKeys57CNC_ID secret as environment variable
+export POKEYS57CNC_ID="${POKEYS57CNC_ID}"
+
+# Export PoKeys57E_ID secret as environment variable
+export POKEYS57E_ID="${POKEYS57E_ID}"
