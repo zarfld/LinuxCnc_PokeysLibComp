3f32bfa4ee661997c93c7add3d32613a7a8ca15b
Dominik Zarfl <13350047+zarfld@users.noreply.github.com>
2025-03-24 15:24:54 +0100

    } else if (joints_in_Sequence == sequence_joints_done) {

--- pokeys_uspace/PokeysCompPulsEngine_base.c
+++ pokeys_uspace/PokeysCompPulsEngine_base.c
@@ -1535,7 +1535,7 @@ int32_t PEv2_HomingStateSyncedTrigger(sPoKeysDevice *dev, int seq, pokeys_home_s
     if (joints_in_Sequence == 0) {
         rtapi_print_msg(RTAPI_MSG_ERR, "PoKeys: %s:%s: No axes in sequence %d\n", __FILE__, __FUNCTION__, seq);
         return 1; // no axes in sequence
-    } else if (joints_in_Sequence != sequence_joints_done) {
+    } else if (joints_in_Sequence == sequence_joints_done) {
         rtapi_print_msg(RTAPI_MSG_ERR, "PoKeys: %s:%s: All axes in sequence %d have NextState %d already (required state %d)\n", __FILE__, __FUNCTION__, seq, NextState, RequiredState);
         return 0; // all joints have NextState already
     } else if (joints_in_Sequence != sequence_joints_ready) {
