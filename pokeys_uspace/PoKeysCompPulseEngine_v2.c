74af960532f105ed0f41e34c13d8f01066684f5a
Dominik Zarfl <13350047+zarfld@users.noreply.github.com>
2025-04-03 10:21:45 +0200

    case PK_PEAxisCommand_axHOMINGFinalize:

--- pokeys_uspace/PoKeysCompPulseEngine_v2.c
+++ pokeys_uspace/PoKeysCompPulseEngine_v2.c
@@ -924,8 +924,10 @@ void PKPEv2_Update(sPoKeysDevice *dev, bool HAL_Machine_On) {
                      * @see PK_PEAxisCommand_axHOMINGFinalize
                      * @memberof PoKeysHALComponent
                      */
-                    rtapi_print_msg(RTAPI_MSG_ERR, "PoKeys: %s:%s: PK_PEAxisCommand_axHOMINGFinalize\n", __FILE__, __FUNCTION__);
-                    if (dev->PEv2.AxesState[i] == PK_PEAxisState_axHOME || intAxesState == PEAxisStateEx_axReadyToFinalizeHoming) {
+                    if (old_PEv2_AxesCommand[i] != *(PEv2_data->PEv2_AxesCommand[i])) {
+                        rtapi_print_msg(RTAPI_MSG_ERR, "PoKeys: %s:%s: PK_PEAxisCommand_axHOMINGFinalize\n", __FILE__, __FUNCTION__);
+                    }
+                    if (dev->PEv2.AxesState[i] == PK_PEAxisState_axHOME || intAxesState == PEAxisStateEx_axReadyToFinalizeHoming || intAxesState == PK_PEAxisState_axREADY) {
                         int MyHomeSequ, seq;
                         MyHomeSequ = PEv2_data->PEv2_home_sequence[i];
 
